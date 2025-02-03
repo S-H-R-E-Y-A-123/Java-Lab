315aec493a8db2eede61c10b7dac9230851a976a
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-02-03 18:30:54 +0530

    Added ConsecutiveDiff method to find smallest difference

--- EvenOrOdd/ArrayFunction.java
+++ EvenOrOdd/ArrayFunction.java
@@ -25,4 +25,30 @@ class ArrayFunctions {
         System.out.println("Even numbers: " + even);
         System.out.println("Odd numbers: " + odd);
     }
+
+    // Function to find two neighboring numbers with the smallest difference
+    int ConsecutiveDiff() {
+        userInput inputHandler = new userInput();
+        int[] numbers = inputHandler.arrayInput();
+
+        if (numbers.length < 2) {
+            System.out.println("Array must have at least two elements.");
+            return -1;
+        }
+
+        Arrays.sort(numbers);
+        int minDiff = Integer.MAX_VALUE;
+        int minIndex = -1;
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int diff = Math.abs(numbers[i + 1] - numbers[i]);
+            if (diff < minDiff) {
+                minDiff = diff;
+                minIndex = i;
+            }
+        }
+
+        System.out.println("Smallest difference is between index " + minIndex + " and " + (minIndex + 1));
+        return minIndex;
+    }
 }
