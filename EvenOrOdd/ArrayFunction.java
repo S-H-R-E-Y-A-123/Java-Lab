4c85944e171a5bf4ad427a69436f19f835797a25
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-02-03 18:30:05 +0530

    Added evenOdd method to classify numbers

--- EvenOrOdd/ArrayFunction.java
+++ EvenOrOdd/ArrayFunction.java
@@ -6,4 +6,23 @@ class ArrayFunctions {
     void display(List<Integer> array) {
         System.out.println("Array: " + array);
     }
+
+    // Function to classify even and odd numbers into separate arrays
+    void evenOdd() {
+        userInput inputHandler = new userInput();
+        int[] numbers = inputHandler.arrayInput();
+        List<Integer> even = new ArrayList<>();
+        List<Integer> odd = new ArrayList<>();
+
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                even.add(number);
+            } else {
+                odd.add(number);
+            }
+        }
+
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
+    }
 }
