737b9c3efa4c4ecc42517208381979dea24095da
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-02-03 18:29:04 +0530

    Added display method to print an array

--- EvenOrOdd/ArrayFunction.java
+++ EvenOrOdd/ArrayFunction.java
@@ -1,4 +1,9 @@
 import java.util.*;
 
 class ArrayFunctions {
+
+    // Function to display an array
+    void display(List<Integer> array) {
+        System.out.println("Array: " + array);
+    }
 }
