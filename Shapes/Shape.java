89a825099a5c80988d02808bea48185aad827098
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-03-01 15:44:14 +0530

    create class

--- Shapes/Shape.java
+++ Shapes/Shape.java
@@ -1,1 +1,10 @@
+// Abstract class Shape, serves as the base class for all shapes.
+abstract class Shape {
+    protected double dim_one, dim_two, dim_three;  
+    protected String shapeType;
 
+    // Constructor to initialize the shape type
+    public Shape(String shapeType) {
+        this.shapeType = shapeType;
+    }
+}
