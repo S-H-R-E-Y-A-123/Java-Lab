2e7b65ba9fe9e1a0bd12d195622b1fc982ed76eb
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-03-01 15:47:10 +0530

    create rectangle class

--- Shapes/Rectangle.java
+++ Shapes/Rectangle.java
@@ -1,1 +1,9 @@
-
+// Rectangle class inherits from Shape
+class Rectangle extends Shape {
+    // Constructor to initialize length and width
+    public Rectangle(double length, double width) {
+        super("Rectangle");
+        this.dim_one = length;
+        this.dim_two = width;
+    }
+}
