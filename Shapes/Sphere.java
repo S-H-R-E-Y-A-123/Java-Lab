12558ca66daf172cf82d39f08a4b91d0c2a9733b
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-03-01 15:54:42 +0530

    Create Sphere.java

---
+++ Shapes/Sphere.java
@@ -0,0 +1,8 @@
+// Sphere class inherits from Shape
+class Sphere extends Shape {
+    // Constructor to initialize the radius
+    public Sphere(double radius) {
+        super("Sphere");
+        this.dim_one = radius;
+    }
+}
