ac360409864fe7348e1b2b8bb01d749f3cca83fc
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-03-01 12:43:06 +0530

    added getters and setters
    "getters" are methods used to retrieve the value of a private variable within a class, while "setters" are methods used to modify or update the value of that same private variable, essentially providing controlled access to data within an object

--- StudentOperation/Student.java
+++ StudentOperation/Student.java
@@ -14,4 +14,55 @@ class Student{
 		this.dept = dept;
 		this.batch = batch;
 	}
+
+	//Getter for NAME
+	public String getName(){
+		return name;
+	}
+
+	//setter for name
+	public void setName(String name){
+		this.name = name;
+	}
+
+	//getter for PRN
+	public long getPRN(){
+		return prn;
+	}
+
+	//setter for PRN
+	public void setPRN(long prn){
+		this.prn = prn;
+	}
+
+	//getter for CGPA
+	public double getCGPA(){
+		return CGPA;
+	}
+
+	//setter for CGPA
+	public void setCGPA(double CGPA){
+		this.CGPA = CGPA;
+	}
+
+	//getter for dept
+	public String getDept(){
+		return dept;
+	}
+
+	//setter for dept
+	public void setDept(String dept){
+		this.dept = dept;
+	}
+
+	//getter for batch 
+	public String getBatch(){
+		return batch;
+	}
+	
+	//setter for batch
+	public void setBatch(String batch){
+		this.batch = batch;
+	}
+
 }
