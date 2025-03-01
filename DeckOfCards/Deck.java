1ce36abc13b6a0e29d6f983ba6f76583d5a4af7e
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-03-01 14:51:14 +0530

    createDeck method added

--- DeckOfCards/Deck.java
+++ DeckOfCards/Deck.java
@@ -1,1 +1,22 @@
+import java.util.*;
 
+class Deck {
+    private ArrayList<Card> deck; // Deck stored as an ArrayList
+
+    public Deck() {
+        deck = new ArrayList<>();
+        createDeck();
+    }
+
+    // Creates a standard deck of 52 cards
+    public void createDeck() {
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        String[] suits = {"Clubs", "Diamonds", "Spades", "Hearts"};
+
+        for (String rank : ranks) {
+            for (String suit : suits) {
+                deck.add(new Card(rank, suit));
+            }
+        }
+    }
+}
