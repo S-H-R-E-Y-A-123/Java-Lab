ccbd5bc89fbbc3d522841698896c0436bb3f9627
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-03-01 12:50:32 +0530

    added main class skeleton

--- StudentOperation/Main.java
+++ StudentOperation/Main.java
@@ -1,1 +1,10 @@
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        StudentOperations operations = new StudentOperations();
+        // Menu-driven system to be added
+    }
+}
 
