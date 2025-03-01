8f52c86259877cc148b1b671d644882725b24b9b
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-03-01 17:43:52 +0530

    Create Duck.java

---
+++ Duck/Duck.java
@@ -0,0 +1,27 @@
+abstract public class Duck {
+    QuackBehaviour quackBehaviour;
+    SwimBehaviour swimBehaviour;
+    FlyBehaviour flyBehaviour;
+
+    public void setSwimBehaviour(SwimBehaviour swimBehaviour){
+        this.swimBehaviour = swimBehaviour;
+    }
+
+    public void setQuackBehaviour(QuackBehaviour quackBehaviour) {
+        this.quackBehaviour = quackBehaviour;
+    }
+
+    public void setFlyBehaviour(FlyBehaviour flyBehaviour) { this.flyBehaviour = flyBehaviour; }
+
+    abstract void display();
+
+    public void performQuack(){
+        quackBehaviour.quack();
+    }
+
+    public void performSwim(){
+        swimBehaviour.swim();
+    }
+
+    public void performFly() { flyBehaviour.fly(); }
+}
