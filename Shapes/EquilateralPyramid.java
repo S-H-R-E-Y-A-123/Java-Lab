e59647c996e3d44f45dcd935c7346dff9c177a27
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-03-01 16:00:15 +0530

    area method added

--- Shapes/EquilateralPyramid.java
+++ Shapes/EquilateralPyramid.java
@@ -6,4 +6,11 @@ class EquilateralPyramid extends Shape {
         this.dim_one = baseSide;
         this.dim_two = height;
     }
+
+    // Method to calculate the surface area of an equilateral pyramid
+    @Override
+    public double calculateArea() {
+        double slantHeight = Math.sqrt((dim_one / 2) * (dim_one / 2) + dim_two * dim_two);
+        return dim_one * dim_one + 2 * dim_one * slantHeight;
+    }
 }
