0847b094029925022f6e9a5c2090c819dc47be5a
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-03-01 15:52:31 +0530

    Create Circle.java

---
+++ Shapes/Circle.java
@@ -0,0 +1,8 @@
+// Circle class inherits from Shape
+class Circle extends Shape {
+    // Constructor to initialize the radius
+    public Circle(double radius) {
+        super("Circle");
+        this.dim_one = radius;
+    }
+}
