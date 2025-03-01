5cd27a0d1a80713caa718b4dde7de4cc9d4a6adf
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-03-01 15:48:43 +0530

    Create Square.java

---
+++ Shapes/Square.java
@@ -0,0 +1,7 @@
+// Square class inherits from Shape
+class Square extends Shape {
+    // Constructor to initialize the side length
+    public Square(double side) {
+        super("Square");
+        this.dim_one = side;
+    }
