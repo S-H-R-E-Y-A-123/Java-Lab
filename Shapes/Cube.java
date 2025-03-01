db73e56f47244c8326d98ac57b15e2a22bc2f8d3
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-03-01 15:50:26 +0530

    Create Cube.java

---
+++ Shapes/Cube.java
@@ -0,0 +1,7 @@
+// Cube class inherits from Shape
+class Cube extends Shape {
+    // Constructor to initialize the side length
+    public Cube(double side) {
+        super("Cube");
+        this.dim_one = side;
+    }
