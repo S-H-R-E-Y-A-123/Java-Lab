a88c42b86b2c866704926c128cd3233c3573a36e
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-04-20 00:04:04 +0530

    Create Student.java

---
+++ StudentData/Student.java
@@ -0,0 +1,26 @@
+public class Student {
+    private String prn;
+    private String name;
+    private String dob;
+    private int marks;
+
+    public Student(String prn, String name, String dob, int marks) {
+        this.prn = prn;
+        this.name = name;
+        this.dob = dob;
+        this.marks = marks;
+    }
+
+    public String getPrn() { return prn; }
+    public String getName() { return name; }
+    public String getDob() { return dob; }
+    public int getMarks() { return marks; }
+
+    public void setName(String name) { this.name = name; }
+    public void setDob(String dob) { this.dob = dob; }
+    public void setMarks(int marks) { this.marks = marks; }
+
+    public void display() {
+        System.out.println("PRN: " + prn + ", Name: " + name + ", DoB: " + dob + ", Marks: " + marks);
+    }
+}
