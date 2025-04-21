edfd3891b3acfe45f89ed731b447f3e2f56582e0
Shreya Chaturvedi <shreya18.chaturvedi@gmail.com>
2025-04-21 20:31:39 +0530

    Create Main.java

---
+++ Threading/Main.java
@@ -0,0 +1,19 @@
+//Shreya Chaturvedi, 23070126123, AIML B2
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main thread started.");
+        
+        
+        //creating the objects from the MyThread.java class
+        MyThread thread1 = new MyThread("Thread-1");
+        MyThread thread2 = new MyThread("Thread-2");
+        
+        
+        //starting those threads
+        thread1.start();
+        thread2.start();
+
+        System.out.println("Main thread ended.");
+    }
+}
